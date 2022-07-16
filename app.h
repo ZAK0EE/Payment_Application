73c79b29a559f5e4d4cc72e389d929a5d0b68536
Ziad <82421473+ZAK0EE@users.noreply.github.com>
2022-07-16 02:28:10 +0200

    Init code

--- app.h
+++ app.h
@@ -1,5 +1,6 @@
 #ifndef _APP_
 #define _APP_
 
+void appStart(void);
 
 #endif // _APP_
--- server.h
+++ server.h
@@ -1,5 +1,32 @@
 #ifndef _SERVER_
 #define _SERVER_
 
+#include <inttypes.h>
+#include "card.h"
+#include "terminal.h"
+
+typedef struct ST_transaction_t
+{
+	ST_cardData_t cardHolderData;
+	ST_terminalData_t terminalData;
+	EN_transState_t transState;
+	uint32_t transactionSequenceNumber;
+}ST_transaction;
+
+typedef enum EN_transState_t
+{
+	APPROVED, DECLINED_INSUFFECIENT_FUND, DECLINED_STOLEN_CARD, INTERNAL_SERVER_ERROR
+}EN_transStat_t;
+
+typedef enum EN_serverError_t
+{
+	OK, SAVING_FAILED, TRANSACTION_NOT_FOUND, ACCOUNT_NOT_FOUND, LOW_BALANCE
+}EN_serverError_t;
+
+typedef struct ST_accountsDB_t
+{
+	float balance;
+	uint8_t primaryAccountNumber[20];
+}ST_accountsDB_t;
 
 #endif // _SERVER_
