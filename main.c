f05f670872457eb4196a2d32bc65fb0046bbc834
Ziad <82421473+ZAK0EE@users.noreply.github.com>
2022-07-16 16:41:55 +0200

    test

--- main.c
+++ main.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
-
+#include "card.h"
 int main()
 {
-	printf("Hello, world\n");
+	ST_cardData_t x;
+	if (getCardHolderName(&x) != WRONG_NAME)
+		printf("%s", x.cardHolderName);
+	else
+		printf("WRONG NAME");
+
 }
