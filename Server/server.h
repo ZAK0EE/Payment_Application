2bb8f148030d6d2ec990abaf978ac2ee0271082f
Ziad <82421473+ZAK0EE@users.noreply.github.com>
2022-07-19 14:05:59 +0200

    Fixed enum errors and added functions prototypes

--- Server/server.h
+++ Server/server.h
@@ -5,18 +5,20 @@
 #include "../Card/card.h"
 #include "../Terminal/terminal.h"
 
+typedef enum EN_transState_t
+{
+	APPROVED, DECLINED_INSUFFECIENT_FUND, DECLINED_STOLEN_CARD, INTERNAL_SERVER_ERROR
+}EN_transState_t;
+
 typedef struct ST_transaction_t
 {
 	ST_cardData_t cardHolderData;
 	ST_terminalData_t terminalData;
 	EN_transState_t transState;
 	uint32_t transactionSequenceNumber;
-}ST_transaction;
+}ST_transaction_t;
 
-typedef enum EN_transState_t
-{
-	APPROVED, DECLINED_INSUFFECIENT_FUND, DECLINED_STOLEN_CARD, INTERNAL_SERVER_ERROR
-}EN_transStat_t;
+
 
 typedef enum EN_serverError_t
 {
@@ -28,5 +30,11 @@ typedef struct ST_accountsDB_t
 	float balance;
 	uint8_t primaryAccountNumber[20];
 }ST_accountsDB_t;
+
+EN_transState_t recieveTransactionData(ST_transaction_t* transData);
+EN_serverError_t isValidAccount(ST_cardData_t* cardData);
+EN_serverError_t isAmountAvailable(ST_terminalData_t* termData);
+EN_serverError_t saveTransaction(ST_transaction_t* transData);
+EN_serverError_t getTransaction(uint32_t transactionSequenceNumber, ST_transaction_t* transData);
 
 #endif // _SERVER_
