42d7fa4bf49a5d39ab09bccdf8c66c3efc1def97
Ziad <82421473+ZAK0EE@users.noreply.github.com>
2022-07-19 20:16:41 +0200

    Removed accounts struct

--- Server/server.h
+++ Server/server.h
@@ -25,12 +25,6 @@ typedef enum EN_serverError_t
 	SERVER_OK, SAVING_FAILED, TRANSACTION_NOT_FOUND, ACCOUNT_NOT_FOUND, LOW_BALANCE
 }EN_serverError_t;
 
-typedef struct ST_accountsDB_t
-{
-	float balance;
-	uint8_t primaryAccountNumber[20];
-}ST_accountsDB_t;
-
 EN_transState_t recieveTransactionData(ST_transaction_t* transData);
 EN_serverError_t isValidAccount(ST_cardData_t* cardData);
 EN_serverError_t isAmountAvailable(ST_transaction_t* transData);
