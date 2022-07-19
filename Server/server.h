c1193d86e027b241ee6df450ba4f3e5da88260f9
Ziad <82421473+ZAK0EE@users.noreply.github.com>
2022-07-19 18:37:39 +0200

    Fixed isAmountAvailable parameter

--- Server/server.h
+++ Server/server.h
@@ -33,7 +33,7 @@ typedef struct ST_accountsDB_t
 
 EN_transState_t recieveTransactionData(ST_transaction_t* transData);
 EN_serverError_t isValidAccount(ST_cardData_t* cardData);
-EN_serverError_t isAmountAvailable(ST_terminalData_t* termData);
+EN_serverError_t isAmountAvailable(ST_transaction_t* transData);
 EN_serverError_t saveTransaction(ST_transaction_t* transData);
 EN_serverError_t getTransaction(uint32_t transactionSequenceNumber, ST_transaction_t* transData);
 
