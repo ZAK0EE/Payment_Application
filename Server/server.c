979558ed5e44d978f76d986ca522581de97a713b
Ziad <82421473+ZAK0EE@users.noreply.github.com>
2022-07-19 19:40:22 +0200

    Added implementation of IsValidAccount

--- Server/server.c
+++ Server/server.c
@@ -1,3 +1,8 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "server.h"
 
 
 ST_accountsDB_t accounts[255];
@@ -21,6 +26,25 @@ EN_transState_t recieveTransactionData(ST_transaction_t* transData);/*
 
 }*/
 
+EN_serverError_t isValidAccount(ST_cardData_t* cardData)
+{
+	FILE* file = NULL;
+	fopen_s(&file, "Accounts DB.txt", "r");
+	if (file == 0)
+		return ACCOUNT_NOT_FOUND;
+
+	char buffer[100] = { 0 };
+
+	// Checking for PAN in DB
+	while (fscanf_s(file, "Card Data: %[^,] %*[^\n]\n", buffer, (unsigned int)_countof(buffer)) != EOF)
+	{
+		if (strcmp(cardData->primaryAccountNumber, buffer) == 0)
+			return SERVER_OK;
+	}
+
+	return DECLINED_STOLEN_CARD;
+
+}
 
 EN_serverError_t isAmountAvailable(ST_transaction_t* transData);
 EN_serverError_t saveTransaction(ST_transaction_t* transData);
